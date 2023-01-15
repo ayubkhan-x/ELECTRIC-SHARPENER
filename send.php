ad75679e2d9711bf9bc3b420fa1e6d19733c8ddc
ayubkhan-x <khakimovayubkhan@gmail.com>
2023-01-15 20:16:26 +0500

    выфвыфв

--- send.php
+++ send.php
@@ -2,4 +2,4 @@ TOKEN="5736702325:AAFI_RO3X1kKIumqCBCvPXwcSOWmS2Ea1NA"
 ID="-1001539398172"
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
-curl -s -X POST $URL -d chat_id=$ID -d text="Hello World"
+curl -s -X POST $URL -d chat_id=$ID -d text="Site"
