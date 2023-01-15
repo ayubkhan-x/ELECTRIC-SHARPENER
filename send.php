f824f3b3aa097c6d7e4fef5e06a600179ce28937
ayubkhan-x <khakimovayubkhan@gmail.com>
2023-01-15 20:24:40 +0500

    dasdas

--- send.php
+++ send.php
@@ -1,5 +1,11 @@
-TOKEN="5736702325:AAFI_RO3X1kKIumqCBCvPXwcSOWmS2Ea1NA"
-ID="-1001539398172"
-URL="https://api.telegram.org/bot$TOKEN/sendMessage"
-
-curl -s -X POST $URL -d chat_id=$ID -d text="Site"
+<?php
+  if(isset($_POST['submitButton'])){
+  $apiToken = "5736702325:AAFI_RO3X1kKIumqCBCvPXwcSOWmS2Ea1NA";
+  $data = [
+    'chat_id' => '-1001539398172', 
+    'text' => $_POST['name'],['phone']
+  ];
+  $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" .
+                                 http_build_query($data) );
+  }    
+?>
